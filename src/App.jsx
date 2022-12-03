fbb21c18a6d2d8b3fc1edca4706fab8bb655d86c
Tamim Ahamed <tamimssgts999@gmail.com>
2022-12-04 01:40:56 +0600

    react & tailwind setup done

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+import DashboardHeader from './components/dashboard-index/DashboardHeader';
+
+function App() {
+  return (
+    <DashboardHeader />
+  );
+}
+
+export default App;
---
+++ src/components/dashboard-index/DashboardHeader.jsx
@@ -0,0 +1,13 @@
+
+const DashboardHeader = () => {
+  return (
+    <div className="">
+        <div className="[&>*]:text-primary">
+            <h3>Pathfinder</h3>
+            <h3>Reloaded</h3>
+        </div>
+    </div>
+  )
+}
+
+export default DashboardHeader
